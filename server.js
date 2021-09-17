bacdef35f6e1934a5005c50aa73c2f9f6d8dd3db
Peter Gibbons <gibbons.peter312@gmail.com>
2021-09-16 19:06:58 -0700

    Created Skeleton
    basic layout of website

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const path = require('path');
+const port = process.env.port || 5000;
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+//possible need to reconfigure session details
+app.use(
+    session({
+        secret: "secret", // rename  secret to secure password
+        reverse: false,
+        saveUninitialized: false,
+        cookie: {
+            httpOnly: true,
+            secure: false,
+            maxAge: 24 * 60 * 60 * 1000,
+        },
+    })
+);
+
+const authRoute = require("./routes/authRoute");
+const indexRoute = require("./routes/indexRoute");
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next) => {
+    console.log(req.session.passport);
+    next();
+});
+
+app.use("/", indexRoute);
+app.use("/auth", authRoute);
+
+// database deatils go here
+
+app.listen(port, () => {
+    console.log(`Server start on port ${port}`)
+})
