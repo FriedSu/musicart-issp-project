2023e55b27ee5b099834c273cd1375ec63903e4b
jwarhurst22 <jawarhurst@hotmail.com>
2021-09-17 12:57:22 -0700

    register-skeleton

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 //Routes for authentication 
 router.get("/login",(req, res) => res.render("login"));
+router.get("/register", (req, res) => res.render("register"))
 
 module.exports = router;
