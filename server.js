e35ef467e5388446c604269939dfb86c4a98a6e6
Sydney L <riko1887@gmail.com>
2021-10-14 22:29:56 -0700

    Attempt #1 for success page

--- server.js
+++ server.js
@@ -66,7 +66,8 @@ app.get("/checkout", (req, res) => {
 app.get("/payment_success", (req, res) => res.render('payment_success'));
 app.get("/payment_cancel", (req, res) => res.render('payment_cancel'));
 app.get('/checkout-session', async (req, res) => {
-    const session = await stripe.checkout.sessions.retrieve(req.query.id)
+    const session = await stripe.checkout.sessions.retrieve(req.query.id, {
+        });
         res.json({session});
     });
 
