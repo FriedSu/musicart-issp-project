38a5eb902e6a3cb2c3d03030a934b88c0db93c15
Peter Gibbons <gibbons.peter312@gmail.com>
2021-11-15 10:48:28 -0800

    re added bycrpt

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,5 @@
-
 const mongoose = require('mongoose');
+const bcrypt = require("bcryptjs");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema ({
@@ -18,12 +18,31 @@ const userSchema = new Schema ({
     role: {
         type: String,
         default: 'User',
-    },
-    profilePicture: {
-        type: String,
-        default: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Logo_musicart_group_logo_cmyk.png/1200px-Logo_musicart_group_logo_cmyk.png',
     }
 }, { timestamps: true });
+
+userSchema.pre("save", function (next) {
+    const user = this
+  
+    if (this.isModified("password") || this.isNew) {
+      bcrypt.genSalt(10, function (saltError, salt) {
+        if (saltError) {
+          return next(saltError)
+        } else {
+          bcrypt.hash(user.password, salt, function(hashError, hash) {
+            if (hashError) {
+              return next(hashError)
+            }
+  
+            user.password = hash
+            next()
+          })
+        }
+      })
+    } else {
+      return next()
+    }
+  })
 
 const User = mongoose.model('User', userSchema)
 
