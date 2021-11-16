a1ebe3a30c41823e2f3adba99ebb42e1680ec16e
jwarhurst22 <jawarhurst@hotmail.com>
2021-11-16 10:54:55 -0800

    feature tweaking

--- models/userModel.js
+++ models/userModel.js
@@ -18,7 +18,12 @@ const userSchema = new Schema ({
     role: {
         type: String,
         default: 'User',
-    }
+    },
+    profilePicture: {
+      type: String,
+      default: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Logo_musicart_group_logo_cmyk.png/1200px-Logo_musicart_group_logo_cmyk.png',
+  }
+
 }, { timestamps: true });
 
 userSchema.pre("save", function (next) {
